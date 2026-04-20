f92c6c4d28b57a6fbc65a1c43233e180d061ab61
AntonioZerbato <antozerba28@gmail.com>
2026-04-20 10:37:14 +0200

    002: controller first impl

--- src/controller.cpp
+++ src/controller.cpp
@@ -7,17 +7,42 @@
 
 
 #include "assignment1_rt2/action/target.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/twist.h"
+
+//using for semplicity
+using Target = assignment1_rt2::action::Target;
+
 
 namespace target_controller{
 class TargetController : public rclcpp::Node{
     public:
-    //using for semplicity
-    using Target = assignment1_rt2::action::Target;
 
     explicit TargetController(const rclcpp::NodeOptions & options = rclcpp::NodeOptions() ) : 
     Node("target_controller", options){
 
-        
+        //Action 
+
+        this-> action_server = rclcpp_action::create_server<Target>(
+            this,
+            "target",
+            std::bind(&TargetController::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&TargetController::handle_cancel, this, std::placeholders::_1),
+            std::bind(&TargetController::handle_accepted, this, std::placeholders::_1)
+        );
+
+        //Frame Listerner
+        tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener  = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
+        //VelPub
+        vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+
+
+
+
 
 
     }
@@ -25,12 +50,17 @@ class TargetController : public rclcpp::Node{
 
     private:
     rclcpp_action::Server<Target>::SharedPtr action_server;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener{};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub;
+
 
     //goal 
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID & uuid,
         std::shared_ptr<const Target::Goal> goal){
-            RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->order);
+            RCLCPP_INFO(this->get_logger(), "Received goal: X: %f, Y: %f, Theta: %f",
+             goal->target_pose[0], goal->target_pose[1], goal->target_pose[2]);
             (void)uuid;
             return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
         }
@@ -53,6 +83,7 @@ class TargetController : public rclcpp::Node{
         const auto goal = goal_handle->get_goal();
         auto feedback = std::make_shared<Target::Feedback>();
         auto result = std::make_shared<Target::Result>();
+
 
         //implementation of action logic
     }
--- src/ui_node.cpp
+++ src/ui_node.cpp
@@ -4,22 +4,103 @@
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/static_transform_broadcaster.h"
+#include "assignment1_rt2/action/target.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
+//using for semplicityi
+using Target = assignment1_rt2::action::Target;
+using TargetHandle = rclcpp_action::ClientGoalHandle<Target>;
+using namespace std::placeholders;
 
-class TargetInferface  : public rclcpp::Node{
+class TargetInterface  : public rclcpp::Node{
     public: 
-    TargetInferface() : Node("target_interface"){
+    TargetInterface() : Node("target_interface"){
+        
+
+        //broadcaster
         target_broadcaster =  std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
         //publish targret pos
         this->get_input();
         this->make_target();
 
-        //sleep for rviz 
-        rclcpp::sleep_for(std::chrono::seconds(1));
+        //client
+        this->action_client = rclcpp_action::create_client<Target>(this, "target");
+
+        //timer called once, stopped in send_goal()
+        this->timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500),
+            std::bind(&TargetInterface::send_goal, this));
+
+
+
     };
 
     private: 
+    void send_goal(){
+        timer_->cancel(); //stop timer after first call
+
+        if(!this->action_client->wait_for_action_server(std::chrono::seconds(10))){
+            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+            rclcpp::shutdown(); //close node if no action server available
+            return;
+        }
+        //creazione goal
+        auto goal = Target::Goal();
+        goal.target_pose.push_back(x);
+        goal.target_pose.push_back(y);
+        goal.target_pose.push_back(theta);
+        RCLCPP_INFO(this->get_logger(), "Sending goal");
+        auto send_goal_options = rclcpp_action::Client<Target>::SendGoalOptions();
+        send_goal_options.goal_response_callback = 
+            std::bind(&TargetInterface::goal_response_callback, this, _1);
+        send_goal_options.feedback_callback = 
+            std::bind(&TargetInterface::feedback_callback, this, _1, _2);
+        send_goal_options.result_callback = 
+            std::bind(&TargetInterface::result_callback, this, _1);
+        this->action_client->async_send_goal(goal, send_goal_options);
+
+        
+
+    }
+
+    void goal_response_callback(std::shared_ptr<TargetHandle> goal_handle)
+    {
+        if (!goal_handle) {
+        RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        } else {
+        RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    void feedback_callback(TargetHandle::SharedPtr,
+        const std::shared_ptr<const Target::Feedback> feedback)
+    {
+        //logging partial pose value
+        RCLCPP_INFO(this->get_logger(), "Partial Pose X: %f, Y: %f, Theta: %f", 
+                feedback->partial_pose[0], feedback->partial_pose[1], feedback->partial_pose[2]);
+    }
+
+    void result_callback(const TargetHandle::WrappedResult & result)
+    {
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+        //logging final pose value
+        RCLCPP_INFO(this->get_logger(), "Final Pose X: %f, Y: %f, Theta: %f", 
+                result.result->final_pose[0], result.result->final_pose[1], result.result->final_pose[2]);
+    }
+
     void get_input(){
         std::cout << "Enter target position (x y theta): ";
         std::cin >> x >> y >> theta;
@@ -29,7 +110,7 @@ class TargetInferface  : public rclcpp::Node{
         geometry_msgs::msg::TransformStamped target_transform;
         target_transform.header.stamp = this->get_clock()->now();
         target_transform.header.frame_id = "base_link"; //father link
-        target_transform.child_frame_id = "target";
+        target_transform.child_frame_id = "target"; //child link
 
         target_transform.transform.translation.x = x;
         target_transform.transform.translation.y = y;
@@ -49,6 +130,9 @@ class TargetInferface  : public rclcpp::Node{
     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> target_broadcaster;
 
     //TODO: client for action
+    rclcpp_action::Client<Target>::SharedPtr action_client;
+    rclcpp::TimerBase::SharedPtr timer_;
+
 
 
 
@@ -57,7 +141,7 @@ class TargetInferface  : public rclcpp::Node{
 
 int main(int argc, char* argv[]){
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TargetInferface>());
+    rclcpp::spin(std::make_shared<TargetInterface>());
     rclcpp::shutdown();
     return 0;
 }
